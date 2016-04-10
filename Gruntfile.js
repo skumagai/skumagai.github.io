afbec4a3fbef058039ef6e1cff529188c99d2745
Seiji Kumagai <seiji.kumagai@gmail.com>
2016-04-10 23:55:29 +0200

    Add the second set of slides.

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,7 +129,7 @@ module.exports = function(grunt) {
 				tasks: 'css-core'
 			},
 			html: {
-				files: [ 'index.html']
+				files: [ 'index.html', 'slides1.html', 'slides2.html' ]
 			},
 			markdown: {
 				files: [ './*.md' ]
